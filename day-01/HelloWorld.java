12462c3a9510788afd072c705033ae06398fae93
Yash Ladlapure <yash.ladlapure@mitwpu.edu.in>
2026-06-20 23:00:10 +0530

    refactor: clean up Day 1 Java practice files

--- day-01/HelloWorld.java
+++ day-01/HelloWorld.java
@@ -1,6 +1,3 @@
-// Day 01 - Hello World
-// The first Java program
-
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
--- day-01/InputOutput.java
+++ day-01/InputOutput.java
@@ -1,26 +1,18 @@
-// Day 01 - User Input and Output
-// Takes name and age from user and prints a greeting
-
 import java.util.Scanner;
 
 public class InputOutput {
     public static void main(String[] args) {
-        // Create a Scanner object to read input
         Scanner scanner = new Scanner(System.in);
 
-        // Ask for name
         System.out.print("Enter your name: ");
         String name = scanner.nextLine();
 
-        // Ask for age
         System.out.print("Enter your age: ");
         int age = scanner.nextInt();
 
-        // Print greeting
         System.out.println("Hello, " + name + "! You are " + age + " years old.");
         System.out.println("Welcome to Java programming!");
 
-        // Close scanner
         scanner.close();
     }
 }
